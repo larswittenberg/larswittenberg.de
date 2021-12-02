4d2450c4f0348edeedc34be25cd0add601361d9d
Lars Wittenberg <larswittenberg@googlemail.com>
2021-12-03 00:35:45 +0100

    UPDATE: Index Page

--- components/LayoutDefault.js
+++ components/LayoutDefault.js
@@ -1,11 +1,19 @@
+import cn from 'classnames'
 import Header from './Header'
 import Footer from './Footer'
 
-export default function LayoutDefault({ className, children }) {
+export default function LayoutDefault({ children, fullWidth, centerVertical }) {
+	let ClassNames = cn({
+		'flex-grow mx-4 lg:mx-auto text-2xl': true,
+		'lg:w-11/12 lg:mx-auto': fullWidth,
+		'max-w-prose': !fullWidth,
+		'flex items-center': centerVertical
+	})
+
 	return (
 		<div className="min-h-screen flex flex-col">
-			<Header className="" />
-			<main className={`flex-grow mx-4 lg:mx-auto max-w-prose text-2xl ${className}`}>
+			<Header />
+			<main className={ClassNames}>
 				{children}
 			</main>
 			<Footer />
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
+import NextImage from 'next/image'
 import LayoutDefault from '../components/LayoutDefault'
 
 export default function IndexPage() {
 	return (
-		<LayoutDefault>
-			<h1 className="text-5xl text-center mb-12">
-				Hi
-			</h1>
+		<LayoutDefault fullWidth centerVertical>
+			<div className="mx-32">
+				<p className="text-8xl font-extrabold">
+					Hi!{' '}<span role="img" aria-label="Friedenszeichen Emoji">✌️</span>
+				</p>
+				<h1 className="text-8xl font-extrabold leading-none mb-12">
+					Ich bin Lars Wittenberg.<br />
+					<small className="text-6xl ">Frontend Web Developer aus Stuttgart.</small>
+				</h1>
+			</div>
 		</LayoutDefault>
 	)
 }
