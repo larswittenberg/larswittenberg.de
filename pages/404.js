aec09491fda351abacd472c9facb4ba638e1a6b1
Lars Wittenberg <larswittenberg@googlemail.com>
2022-02-22 23:55:56 +0100

    UPDATE: 404 Page

--- pages/404.js
+++ pages/404.js
@@ -3,8 +3,8 @@ import LayoutDefault from '../components/LayoutDefault'
 export default function IndexPage() {
 	return (
 		<LayoutDefault fullWidth centerVertical>
-			<div className="mx-32">
-				<h1 className="text-8xl font-extrabold leading-none mb-12">
+			<div className="mx-auto max-w-[600px] md:max-w-none">
+				<h1 className="text-3xl sm:text-5xl lg:text-6xl font-extrabold leading-none mb-12">
 					404
 					<br />
 					Seite nicht gefunden 😢
--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import SocialIcons from '../components/SocialIcons'
 export default function IndexPage() {
 	return (
 		<LayoutDefault fullWidth centerVertical>
-			<div className="mx-auto max-w-[460px] md:max-w-none  font-extrabold">
+			<div className="mx-auto max-w-[460px] md:max-w-none font-extrabold">
 				<p className="text-4xl sm:text-6xl md:text-8xl">
 					Hi!{' '}
 					<span role="img" aria-label="Friedenszeichen Emoji">
 						✌️
 					</span>
 				</p>
-				<h1 className="text-3xl sm:text-5xl lg:text-6xl  leading-none mb-12">
+				<h1 className="text-3xl sm:text-5xl lg:text-6xl leading-none mb-12">
 					Ich bin Lars Wittenberg.
 					<br />
 					<small className="text-xl sm:text-4xl lg:text-5xl">
