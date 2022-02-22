de1ee9fc9d48aada7988d3244965e8d557b15255
Lars Wittenberg <larswittenberg@googlemail.com>
2022-02-23 00:59:47 +0100

    FIX: Footer Color for better A11y

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
 	return (
 		<footer className="lg:w-11/12 mt-16 mx-4 md:mx-10 lg:mx-auto py-8 border-t border-solid border-gray-700 text-center">
 			<div>
-				<p className="text-base text-gray-500">
+				<p className="text-base text-gray-400">
 					© {new Date().getFullYear()} Lars Wittenberg · Built with{' '}
 					<a href="https://nextjs.org/" className={linkClass}>
 						next.js
