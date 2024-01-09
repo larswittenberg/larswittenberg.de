9b663b0443a147f16cdcd25d01c69c66eb627bab
Lars Wittenberg <larswittenberg@googlemail.com>
2024-01-09 19:00:30 +0100

    CONTENT:  Update uses page

--- pages/uses.js
+++ pages/uses.js
@@ -37,7 +37,7 @@ export async function getStaticProps() {
 const source = `---
 title: 'Uses'
 date: '2020-06-03'
-update: '2022-11-16'
+update: '2024-01-09'
 desc: ''
 ---
 
@@ -48,26 +48,32 @@ Inspiriert von [Wes Bos](https://wesbos.com/uses/) und durch sein [uses.tech Pro
 
 Auf Github habe ich ein [Repository mit meinem macOS-Setup](https://github.com/larswittenberg/mac-setup) samt detailierten Einstellungen und Programmen veröffentlicht.
 
+<Figure>
+	<Image src="/images/schreibtisch.jpg" width="1920" height="1464" alt="Mein Homeoffice Schreibtisch-Setup" />
+</Figure>
 
 ## Hardware
 
 ### Privat (Homeoffice)
 - [LG 4K Monitor 27" mit USB-C](https://www.amazon.de/gp/product/B07MKT1W65)
-- [BenQ 24" Monitor im Hochformat als second screen](https://www.amazon.de/gp/product/B00UAMYTR8)
+- [BenQ 24" Monitor im Hochformat als zweiter Monitor](https://www.amazon.de/gp/product/B00UAMYTR8)
 - [Logitech MX Keys Tastatur](https://www.amazon.de/Logitech-Keys-fortschrittliche-Batterielebensdauer-Metallaufbau/dp/B07W7LMCF9/)
 - [Logitech MX Master Maus](https://www.amazon.de/Logitech-MX-Master-AMZ-Kabellose-Bluetooth-Windows-schwarz/dp/B0761YL588/)
+- [Anker PowerConf C200 2K USB-Webcam](https://www.amazon.de/gp/product/B09MFMTMPD)
+- [Elgato Stream Deck](https://www.elgato.com/de/de/p/stream-deck-mk2-black)
 - Synology NAS DS216play
 - Externe SSD mit 512GB [WD My Passport SSD](https://www.amazon.de/Passport-Festplatte-Kennwortschutz-automatische-Datensicherung/dp/B07C5BG857/ref=sr_1_1_sspa)
+- [NoVoo 7 in 1 USB-C Adapter](https://www.amazon.de/gp/product/B0957DFS8V)
 - Nest Wifi-Router
 - LED Schreibtischlampe von [Eyocean](https://www.amazon.de/gp/product/B07RKQMX15/)
-- [Steh-Schreibtisch "Skarsta"](https://www.ikea.com/de/de/p/skarsta-schreibtisch-sitz-steh-weiss-s59324818/) von Ikea in der Größe 120x70 cm
-- Büro-Stuhl: Keiner. Der Steh-Schreibtisch bleibt immer oben.
+- [Vertikal Laptop Ständer](https://www.amazon.de/gp/product/B074PPGHMC)
+- Elektrisch verstellbarer Steh-Schreibtisch ["E7" von Flexispot](https://www.flexispot.de/elektrisch-hohenverstellbares-tischgestell-e7.html)
+- Sitz-/Stehstütze [Lidkullen von IKEA](https://www.ikea.com/de/de/p/lidkullen-sitz-stehstuetze-aktiv-gunnared-dunkelgrau-30445774/) als Hocker
 
 
 ### Beruflich
 - MacBook Pro 14" M1 Max von 2021
 - Dell UltraSharp 34" Curved Monitor
-- Logitech MX Master Maus (ich nutze das gleiche Model im Büro und privat)
 - Monitor-Standfuß aus Holz von [Samdi](https://www.amazon.de/Samdi-H%C3%B6lzerne-Universal-Elegante-Halterung-Schwarze-Walnuss/dp/B0753DCGX1?ref_=ast_bbp_dp)
 
 
@@ -75,9 +81,9 @@ Auf Github habe ich ein [Repository mit meinem macOS-Setup](https://github.com/l
 ## Software
 
 ### Entwicklung
-- [Visual Studio Code](https://code.visualstudio.com/) (~~Sublime Text~~, ~~Atom~~)
-- VS Code Theme: [Cobalt 2](https://marketplace.visualstudio.com/items?itemName=wesbos.theme-cobalt2)
-- VS Code Font: [Hack](https://sourcefoundry.org/hack/)
+- [Visual Studio Code](https://code.visualstudio.com/)
+- [Cobalt 2](https://marketplace.visualstudio.com/items?itemName=wesbos.theme-cobalt2) VS Code Theme
+- [Hack](https://sourcefoundry.org/hack/) VS Code Font
 - [iTerm2](https://iterm2.com/) als Terminal
 - [Tower](https://www.git-tower.com/mac) als Git Client
 - [Github Desktop](https://desktop.github.com/) als Git Client
@@ -93,8 +99,10 @@ Hier nur ein paar als Empfehlung die sich in meinem Alltag bewährt haben.
 - [Shottr](https://shottr.cc/) für mehr Screenshot Funktionen
 - [Clippy](https://clipy-app.com/) als erweiterte Zwischenablage
 - [Tiles](https://www.sempliva.com/tiles/) für Fenster-Management
+- [MarkEdit for Markdown - Text-Editor](https://apps.apple.com/us/app/markedit-for-markdown/id1669953820?mt=12)
 - [NameChanger](https://mrrsoftware.com/namechanger/) schnelle Umbenennung von Dateien
 - [Raycast](https://www.raycast.com/) das bessere Spotlight und besser als Alfred
+- [Rocket Typist - Snippet-Editor](https://www.witt-software.com/rockettypist/)
 
 
 ### Chrome Browser Erweiterungen
@@ -119,12 +127,11 @@ Hier nur ein paar als Empfehlung die sich in meinem Alltag bewährt haben.
 ## Weiteres
 
 - Smartphone Google Pixel 7 Pro
-- Smartwatch Google Pixel Watch
 - Apple iPad Air (4. Gen)
 - iPad Air Tastatur [Logitech Combo Touch](https://www.amazon.de/gp/product/B07W6HP8ZG/)
 - [Bluetooth Noise Cancelling Kopfhörer Sony WH-1000XM3](https://www.amazon.de/gp/product/B07GDR2LYK)
 - [Bluetooth Kopfhörer Anker Soundcore Liberty Air 2](https://www.amazon.de/gp/product/B07ZHDYH6P)
-- [Ortlieb Commuter Daypack Urban](https://www.ortlieb.com/de/commuter-daypack-urban-line+R4155) Rucksack. Wasserdicht, Roll-Schnell-Verschluss, mit Laptop-Fach.
+- [Ortlieb Commuter Daypack Urban](https://www.ortlieb.com/de/commuter-daypack-urban-line+R4155) Rucksack.
 
 
 ## Fahrrad
