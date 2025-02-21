5a97c2d2156e02a5cc6e22e6794c1cbd3f2261d1
Lars Wittenberg <larswittenberg@googlemail.com>
2025-02-21 23:38:45 +0100

    refactor: add files to src folder

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import { DefaultSeo } from 'next-seo'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
-import SEO from '../next-seo.config'
+import SEO from '../../next-seo.config'
 
 export default function App({ Component, pageProps }) {
 	return (
