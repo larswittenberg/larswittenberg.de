e9847dd04891da4780253c9b65557216e003a833
Lars Wittenberg <larswittenberg@googlemail.com>
2025-02-22 00:26:35 +0100

    refactor: move more pages to app router

---
+++ src/app/impressum/page.tsx
@@ -0,0 +1,9 @@
+import LayoutDefault from '@/components/LayoutDefault';
+
+export default function ImpressumPage() {
+	return (
+		<LayoutDefault prose>
+			<h1>Impressum</h1>
+		</LayoutDefault>
+	);
+}
