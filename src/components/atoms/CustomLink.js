d54bfe9f8aa127f0452c72fe3933c6c0c87f7971
Lars Wittenberg <larswittenberg@googlemail.com>
2025-02-27 23:16:11 +0100

    feat: Update custom link component

--- src/components/atoms/CustomLink.js
+++ src/components/atoms/CustomLink.js
@@ -1,7 +1,23 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-export default function CustomLink({ as, href, ...otherProps }) {
+export default function CustomLink({ href, children, ...props }) {
+	if (href?.startsWith('/')) {
+		return (
+			<Link href={href} className="link" {...props}>
+				{children}
+			</Link>
+		);
+	}
+	if (href?.startsWith('#')) {
+		return (
+			<a href={href} className="link" {...props}>
+				{children}
+			</a>
+		);
+	}
 	return (
-		<Link as={as} href={href} {...otherProps} />
-	)
+		<a href={href} target="_blank" rel="noopener noreferrer" className="link" {...props}>
+			{children}
+		</a>
+	);
 }
