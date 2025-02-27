8829b444e23ee239e3709d939ec367f205447fee
Lars Wittenberg <larswittenberg@googlemail.com>
2025-02-27 22:25:35 +0100

    feat: Update PostMetaInfos

--- src/components/PostMetaInfos.tsx
+++ src/components/PostMetaInfos.tsx
@@ -1,7 +1,7 @@
-export default function PostMetaInfos({ date, update }: { date: string; update?: string }) {
+export default function PostMetaInfos({ date, update, showText }: { date: string; update?: string, showText?: boolean }) {
 	return (
 		<p className="mb-12 text-base">
-			Veröffentlich am:{' '}
+			{showText && `Veröffentlich am: `}
 			{new Intl.DateTimeFormat('de-DE', {
 				year: 'numeric',
 				month: 'long',
