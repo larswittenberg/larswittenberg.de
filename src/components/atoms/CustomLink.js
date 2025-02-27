24fa910446e7ea8ecfb0a93a3083521e53b1f250
Lars Wittenberg <larswittenberg@googlemail.com>
2025-02-27 21:57:05 +0100

    fix: update mdx-wrapper

--- src/components/atoms/CustomLink.js
+++ src/components/atoms/CustomLink.js
@@ -2,8 +2,6 @@ import Link from 'next/link'
 
 export default function CustomLink({ as, href, ...otherProps }) {
 	return (
-		<>
-			<Link as={as} href={href} {...otherProps} />
-		</>
+		<Link as={as} href={href} {...otherProps} />
 	)
 }
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -4,30 +4,22 @@
 import type { MDXComponents } from 'mdx/types';
 import Image, { ImageProps } from 'next/image';
 import CustomFigure from '@/components/atoms/CustomFigure';
+import CustomLink from '@/components/atoms/CustomLink'
+import FlexWrapper from '@/components/atoms/FlexWrapper'
 
 // This file allows you to provide custom React components
 // to be used in MDX files. You can import and use any
 // React component you want, including inline styles,
 // components from other libraries, and more.
-
 export function useMDXComponents(components: MDXComponents): MDXComponents {
 	return {
 		// Allows customizing built-in components, e.g. to add styling.
 		// h1: ({ children }) => <h1 style={{ color: 'red', fontSize: '48px' }}>{children}</h1>,
 		img: (props) => <Image sizes="100vw" style={{ width: '100%', height: 'auto' }} {...(props as ImageProps)} />,
+
 		Figure: CustomFigure,
+		FlexWrapper,
+		a: CustomLink,
 		...components,
 	};
 }
-
-
-// import Image from 'next/image';
-// import CustomLink from '@/components/atoms/CustomLink'
-// import FlexWrapper from '@/components/atoms/FlexWrapper'
-
-// const components = {
-// 	Image,
-// 	FlexWrapper,
-// 	a: CustomLink,
-// 	Figure: CustomFigure,
-// };
