ed7928d231d387f1d3d8fccfe23c24732272ed46
Lars Wittenberg <larswittenberg@googlemail.com>
2025-12-15 23:58:22 +0100

    feat: prepare update for new mdx setup

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
+const withMDX = require('@next/mdx')({
+			extension: /.mdx?$/,
+			options: {
+				// If you use remark-gfm, you'll need to use next.config.mjs
+				// as the package is ESM only
+				// https://github.com/remarkjs/remark-gfm#install
+				remarkPlugins: [],
+				rehypePlugins: [],
+				// If you use `MDXProvider`, uncomment the following line.
+				// providerImportSource: "@mdx-js/react",
+			},
+		});
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	// https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
 	reactStrictMode: true,
 	// Configure `pageExtensions`` to include MDX files
-	pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-	// Optionally, add any other Next.js config below
+	pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig);
