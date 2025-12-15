d7716b8c503ecb849b788c6d0e88949af5dcc56c
Lars Wittenberg <larswittenberg@googlemail.com>
2025-12-16 00:02:42 +0100

    ci: codemod migration for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,14 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextConfig from 'eslint-config-next';
 import prettier from 'eslint-config-prettier';
 
-export default [
-  ...nextConfig,
-  prettier,
-  {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    rules: {
-      "react/no-unescaped-entities": ["error", {
-        forbid: [">", "}"],
-      }],
-    },
-  }
-];
+export default [...nextCoreWebVitals, ...nextConfig, prettier, {
+  files: ["**/*.{js,jsx,ts,tsx}"],
+  rules: {
+    "react/no-unescaped-entities": ["error", {
+      forbid: [">", "}"],
+    }],
+  },
+}, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
