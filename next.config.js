f51f15181157ffde0d34d7c859d0f148aa87ee55
Lars Wittenberg <larswittenberg@googlemail.com>
2025-12-15 23:42:22 +0100

    fix: update next.config without webpack

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-module.exports = {
-	webpack: (config, { isServer }) => {
-		if (!isServer) {
-			config.resolve.fallback.fs = false
-		}
-		return config
-	},
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	// https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
+	reactStrictMode: true,
+	// Configure `pageExtensions`` to include MDX files
+	pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+	// Optionally, add any other Next.js config below
+};
+
+module.exports = nextConfig;
