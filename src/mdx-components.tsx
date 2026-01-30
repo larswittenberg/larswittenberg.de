cb274c3bc9160e66d9c1ea5a63a3854b74cdca1c
Lars Wittenberg <larswittenberg@googlemail.com>
2026-01-30 22:18:42 +0100

    feat: add mdx utils

---
+++ src/mdx-components.tsx
@@ -0,0 +1,24 @@
+import type { MDXComponents } from 'mdx/types';
+import Image, { ImageProps } from 'next/image';
+import CustomFigure from '@/components/atoms/CustomFigure'
+
+// const components: MDXComponents = {}
+
+const components = {
+	// Allows customizing built-in components, e.g. to add styling.
+	h1: ({ children }) => (
+		<h1 style={{ color: 'red', fontSize: '48px' }}>{children}</h1>
+	),
+	img: (props) => (
+		<Image
+		sizes="100vw"
+		style={{ width: '100%', height: 'auto' }}
+		{...(props as ImageProps)}
+		/>
+	),
+	Figure: CustomFigure,
+} satisfies MDXComponents;
+
+export function useMDXComponents(): MDXComponents {
+	return components;
+}
