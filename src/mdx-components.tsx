7f9a38d05c694ddb11923aa3d3daae965f42f930
Lars Wittenberg <larswittenberg@googlemail.com>
2026-02-05 00:33:02 +0100

    feat: tweets update mdx custom components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -29,18 +29,34 @@ const componentsForTweets = {
 				sizes="100vw"
 				src={`/projekte/twitter-media/${props.src}`}
 				alt={props.alt || ''}
-				style={{ width: '100%', height: 'auto' }}
+				style={{ width: '50%', height: 'auto' }}
 				// {...(props as ImageProps)}
 			/>
 		);
 	},
 	Video: (props) => {
 		return (
-			<video controls style={{ width: '100%', height: 'auto' }} className="aspect-video">
+			<video controls style={{ width: '50%', height: 'auto' }} className="aspect-video">
 				<source src={`/projekte/twitter-media/${props.src}`} />
 			</video>
 		);
 	},
+	blockquote: (props) => {
+		return (
+			<blockquote className="not-prose border-l-4 border-gray-300 pl-4 italic text-tw-prose-quotes">
+				{props.children}
+			</blockquote>
+		);
+	},
+	p: (props) => {
+		return (<p className="text-base" {...props} />);
+	},
+	a: (props) => {
+		return (<a href={props.href} target="_blank" rel="noopener noreferrer" className="underline text-base">{props.children}</a>);
+	},
+	hr: (props) => {
+		return (<hr className="my-8! border-t border-gray-500" {...props} />);
+	}
 } satisfies MDXComponents;
 
 export const mdxComponentsForTweets = componentsForTweets;
