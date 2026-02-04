6ca1ee63e0e852cf558c5f1cd3197c8d147f1287
Lars Wittenberg <larswittenberg@googlemail.com>
2026-02-04 22:51:38 +0100

    feat: update tweets intro page

--- src/app/projekte/tweets/page.tsx
+++ src/app/projekte/tweets/page.tsx
@@ -1,18 +1,39 @@
 import type { Metadata } from 'next';
-import { mdxComponentsForTweets } from '@/mdx-components';
+import Link from 'next/link';
 import LayoutDefault from '@/components/LayoutDefault';
-import MdxContent from './tweets-2023.mdx';
 
 export const metadata: Metadata = {
-	title: 'tweets',
+	title: 'tweets Übersicht',
 };
 
 export default async function Page() {
+	const years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009];
+
+	const linksForYears = years.map((year) => ({
+		href: `/projekte/tweets/tweets-${year}`,
+		label: `${year}`,
+	}));
+
 	return (
-		<LayoutDefault prose>
-			<article>
-				<MdxContent components={mdxComponentsForTweets} />
-			</article>
+		<LayoutDefault prose fullWidth>
+			<h1>twitter Archiv</h1>
+
+			<p>
+				Übersicht meiner Tweets vom Beginn der Nutzung 2009 bis zum Ende durch den Verkauf der Plattform an
+				Elon.
+			</p>
+			<ol className="m-auto my-8 lg:my-0">
+				{linksForYears.map(({ href, label }) => (
+					<li key={`${href}${label}`}>
+						<Link
+							href={href}
+							className="hover:shadow-linkhover dark:hover:text-darkblue px-4 py-1 text-xl font-bold tracking-wider transition-all duration-150 ease-linear sm:px-1"
+						>
+							{label}
+						</Link>
+					</li>
+				))}
+			</ol>
 		</LayoutDefault>
 	);
 }
