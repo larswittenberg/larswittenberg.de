a01200db386e6593f38ef84319f93e2875eba645
Lars Wittenberg <larswittenberg@googlemail.com>
2026-02-06 21:41:02 +0100

    feat: update tweets page with tweets-count

--- src/app/projekte/tweets/page.tsx
+++ src/app/projekte/tweets/page.tsx
@@ -9,6 +9,9 @@ export const metadata: Metadata = {
 export default async function Page() {
 	const years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009];
 
+	// Anzahl der Tweets pro Jahr. Ausgelesen per Browser-Console via `$$('main blockquote').length`
+	const tweetsPerYear = [10, 725, 892, 833, 959, 870, 1182, 1060, 1012, 995, 991, 991, 747, 831, 649];
+
 	const linksForYears = years.map((year) => ({
 		href: `/projekte/tweets/tweets-${year}`,
 		label: `${year}`,
@@ -23,10 +26,11 @@ export default async function Page() {
 				Elon.
 			</p>
 			<p>
-				Hinweis: Die Links zum originalen Tweet funktionieren nicht mehr, da ich meinen Twitter Account zwar immer noch habe, aber die Tweets nicht mehr öffentlich auf der Plattform zugänglich sind.
+				Hinweis: Die Links zum originalen Tweet funktionieren nicht mehr, da ich meinen Twitter Account zwar
+				immer noch habe, aber die Tweets nicht mehr öffentlich auf der Plattform zugänglich sind.
 			</p>
 
-			<ol className="grid grid-cols-3 gap-x-8 my-8 lg:my-0 marker:text-base!">
+			<ol className="my-8 grid grid-cols-2 lg:grid-cols-3 gap-x-8 marker:text-base! lg:my-0">
 				{linksForYears.map(({ href, label }) => (
 					<li key={`${href}${label}`}>
 						<Link
@@ -35,6 +39,9 @@ export default async function Page() {
 						>
 							{label}
 						</Link>
+						<span className="block sm:inline text-sm text-gray-500 dark:text-gray-400 mt-2 sm:mt-0 sm:ml-2">
+							({tweetsPerYear[years.indexOf(parseInt(label))] || 0} Tweets)
+						</span>
 					</li>
 				))}
 			</ol>
